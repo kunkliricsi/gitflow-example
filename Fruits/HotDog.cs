8054cd595dca2070b698f41629ecc386b6016c47
kunkliricsi <kunkli.ricsi@gmail.com>
2019-01-16 23:14:50 +0100

    added instructions

---
+++ Fruits/HotDog.cs
@@ -0,0 +1,9 @@
+namespace gitflow_example.Fruits
+{
+    public class HotDog : FruitBase
+    {
+        public override string Name => nameof(HotDog);
+        public override string Description => "Sneaky fruit";
+        public override string Color => "Transparent";
+    }
+}
--- Fruits/Tomato.cs
+++ Fruits/Tomato.cs
@@ -3,7 +3,7 @@ namespace gitflow_example.Fruits
     public class Tomato : FruitBase
     {
         public override string Name => nameof(Tomato);
-        public override string Description => "Yes I'm a fruit";
+        public override string Description => "Yes, I'm a fruit";
         public override string Color => "Red";
     }
 }
